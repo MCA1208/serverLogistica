9a94a2d98c969cfa7659e0a602135c5b3d2bc9e6
MCA1208 <33843591+MCA1208@users.noreply.github.com>
2024-12-31 09:32:32 -0300

    conn pg

--- conexion/cnn.js
+++ conexion/cnn.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "admin",
+//   database: "TrackingLogistics",
+//   port: "5432",
+// });
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "admin",
-  database: "TrackingLogistics",
-  port: "5432",
+  connectionString:
+    "postgresql://logi_6437_user:N71lYNwpcji4Oye2wJ5cp1WQet6PTLjh@dpg-ctpt9jbtq21c739rhpi0-a.virginia-postgres.render.com/logi_6437",
 });
 
 module.exports = pool;
