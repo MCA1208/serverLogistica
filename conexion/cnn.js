1648ac36c193984b498aedf94deb8fde147508f6
MCA1208 <33843591+MCA1208@users.noreply.github.com>
2025-01-01 12:19:25 -0300

    add conexion prod

--- conexion/cnn.js
+++ conexion/cnn.js
@@ -7,9 +7,18 @@ const { Pool } = require("pg");
 //   database: "TrackingLogistics",
 //   port: "5432",
 // });
+
+//########### KEY Exernal DEV ###########
+// const pool = new Pool({
+//   connectionString:
+//     "postgresql://logi_6437_user:N71lYNwpcji4Oye2wJ5cp1WQet6PTLjh@dpg-ctpt9jbtq21c739rhpi0-a.virginia-postgres.render.com/logi_6437",
+//   ssl: true,
+// });
+
+//########### KEY Exernal PROD ###########
 const pool = new Pool({
   connectionString:
-    "postgresql://logi_6437_user:N71lYNwpcji4Oye2wJ5cp1WQet6PTLjh@dpg-ctpt9jbtq21c739rhpi0-a.virginia-postgres.render.com/logi_6437",
+    "postgresql://logi_6437_user:N71lYNwpcji4Oye2wJ5cp1WQet6PTLjh@dpg-ctpt9jbtq21c739rhpi0-a/logi_6437",
   ssl: true,
 });
 
