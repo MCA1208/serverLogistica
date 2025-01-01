3ba954b0981c008d945de54a8917485a472a99d4
MCA1208 <33843591+MCA1208@users.noreply.github.com>
2025-01-01 12:25:13 -0300

    se quito ssl false

--- conexion/cnn.js
+++ conexion/cnn.js
@@ -19,7 +19,6 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString:
     "postgresql://logi_6437_user:N71lYNwpcji4Oye2wJ5cp1WQet6PTLjh@dpg-ctpt9jbtq21c739rhpi0-a/logi_6437",
-  ssl: true,
 });
 
 module.exports = pool;
